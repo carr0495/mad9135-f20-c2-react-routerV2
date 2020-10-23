50a7582c78ccd3a66e97df64dab1b2c46a58c05a
carr0495@algonquinlive.com <carr0495@algonquinlive.com>
2020-10-23 13:36:18 -0400

    Updates

---
+++ precache-manifest.339e2d1321ebaee96916de998dd0dac2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c400236ddfe941e3c22e712f875d454b",
+    "url": "/mad9135-f20-c2-react-routerV2/index.html"
+  },
+  {
+    "revision": "e83239d91eb71f792589",
+    "url": "/mad9135-f20-c2-react-routerV2/static/css/main.840e8723.chunk.css"
+  },
+  {
+    "revision": "941dfeb1756b459baa93",
+    "url": "/mad9135-f20-c2-react-routerV2/static/js/2.688b530c.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/mad9135-f20-c2-react-routerV2/static/js/2.688b530c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e83239d91eb71f792589",
+    "url": "/mad9135-f20-c2-react-routerV2/static/js/main.4a1ad8d0.chunk.js"
+  },
+  {
+    "revision": "0ca1f7c76f5c97d8cf97",
+    "url": "/mad9135-f20-c2-react-routerV2/static/js/runtime-main.5382a9ae.js"
+  },
+  {
+    "revision": "1d0dccc725ab526344002f68f39e7347",
+    "url": "/mad9135-f20-c2-react-routerV2/static/media/bg1.1d0dccc7.jpg"
+  }
+]);
